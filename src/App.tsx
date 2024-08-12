b583fdcb3a5e1aca8bfa84139ef99c5aa508cfa7
SinaQP <qasempoursina@gmail.com>
2024-08-12 08:37:12 +0330

    remove the static token

--- src/App.tsx
+++ src/App.tsx
@@ -14,9 +14,7 @@ const App = (props: AppProps) => {
    const [user, setUser] = useState<User | null>(null);
    const [loginEnteredNationalCode, setLoginEnteredNationalCode] =
       useState<string>('');
-   const [token, setToken] = useState<string>(
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwibmF0aW9uYWxfY29kZSI6IjI5ODE1MzI1NzEiLCJleHAiOjE3MjMzODgwMjJ9.EkTyydC9dvXhH5yHiLGKU_1PRbOjTilGtjHtKozXnM0',
-   );
+   const [token, setToken] = useState<string>('');
    const [maskedPhoneNumber, setMaskedPhoneNumber] = useState<string>('');
    const [subSystems, setSubsystems] = useState<SubSystem[]>([]);
    const [selectedCharge, setSelectedCharge] =
