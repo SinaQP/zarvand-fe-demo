da67e69a586ab567dc463e08c137912ce9c852c3
SinaQP <qasempoursina@gmail.com>
2024-09-23 11:12:24 +0330

    add this new files

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+import Layout from '../../components/layout';
+import NationalCodeEntry from './nationalCodeEntry';
+
+
+const Login: FC = () => {
+   return <Layout>
+      <NationalCodeEntry />
+   </Layout>;
+};
+
+export default Login;
