4132002dc04dad036962b78e6d7c784c4b16e8fc
SinaQP <qasempoursina@gmail.com>
2024-09-24 09:00:39 +0330

    ui changes and start connecting the apis

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -3,14 +3,27 @@ import Layout from '../../components/layout';
 import NationalCodeEntry from './nationalCodeEntry';
 import ConfirmationEntry from './confirmationEntry';
 import CounterBadge from './counter';
+import { LoginContext } from './context';
 
 
 const Login: FC = () => {
    const [showConfirmationForm, setShowConfirmationForm] = useState(false);
-   return <Layout extraHeaderContent={showConfirmationForm && <ConfirmationEntry />}
-                  headerBadge={showConfirmationForm && <CounterBadge initialCount={45} />}>
-      <NationalCodeEntry setShowConfirmationForm={setShowConfirmationForm}/>
-   </Layout>;
+   const [nationalCode, setNationalCode] = useState<string[]>([]);
+   const [phoneNumber, setPhoneNumber] = useState<string>('');
+   return <LoginContext.Provider
+      value={{
+         setNationalCode,
+         setPhoneNumber,
+         nationalCode,
+         phoneNumber,
+      }}
+   >
+      <Layout extraHeaderContent={showConfirmationForm && <ConfirmationEntry />}
+              headerBadge={showConfirmationForm && <CounterBadge initialCount={45} />}>
+         <NationalCodeEntry setShowConfirmationForm={setShowConfirmationForm} />
+      </Layout>
+   </LoginContext.Provider>;
 };
 
 export default Login;
+
