63e3ba1371460cf300c22d59132aedbc1433b78d
SinaQP <qasempoursina@gmail.com>
2024-09-28 15:58:37 +0330

    main renovation page

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,20 +1,20 @@
-import { FC } from 'react';
-import { Props } from './index.interface';
+import {FC} from 'react';
+import {Props} from './index.interface';
 import styles from './index.module.scss';
 import Header from './header';
-import { LayoutContext } from './layout.context';
+import {LayoutContext} from './layout.context';
 import Footer from './footer';
 
-const Layout: FC<Props> = ({ headerBadge, children, extraHeaderContent, headerClassName }) => {
-   return (
-      <LayoutContext.Provider value={{ headerBadge, extraHeaderContent }}>
-         <div className={styles.layout}>
-            <Header className={headerClassName}/>
-            <main className={styles.main}>{children}</main>
-            <Footer />
-         </div>
-      </LayoutContext.Provider>
-   );
+const Layout: FC<Props> = ({headerBadge, children, extraHeaderContent, headerClassName, className}) => {
+    return (
+        <LayoutContext.Provider value={{headerBadge, extraHeaderContent}}>
+            <div className={styles.layout}>
+                <Header className={headerClassName}/>
+                <main className={`${styles.main} ${className}`}>{children}</main>
+                <Footer/>
+            </div>
+        </LayoutContext.Provider>
+    );
 };
 
 export default Layout;
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,16 @@
-import { FC } from 'react';
+import {FC} from 'react';
 import Layout from '../../components/layout';
 import styles from './index.module.scss';
 
 const Home: FC = () => {
-   return <Layout headerClassName={styles.header}>HOME</Layout>;
+    return <Layout headerClassName={styles.header} className={styles.layout}>
+        <p className={styles['welcome-text']}>
+            محمد حسین عزیز ! خوش آمدید.
+            امیدواریم تجربه‌ای سریع و آسان در مدیریت عوارض شهری داشته باشید.
+        </p>
+
+        <span className={styles['coming-soon-text']}>به زودی</span>
+    </Layout>;
 };
 
 export default Home;
