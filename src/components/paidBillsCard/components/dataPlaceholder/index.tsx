0d096ecda47b47a3c88d6f7c07087361b36c8561
amirhossein-etemadi <a.etemadi2004@gmail.com>
2024-10-26 10:56:13 +0330

    bugfix

--- src/components/paidBillsCard/components/dataPlaceholder/index.tsx
+++ src/components/paidBillsCard/components/dataPlaceholder/index.tsx
@@ -7,8 +7,13 @@ const DataPlaceholder: FC<DataInputProps> = ({ title, data }) => {
    return (
       <div id="dataInputStypeWrapper">
          <span id="title">{title || 'عنوان'}</span>
-         <span>{data}</span>
-         <CopyIcon color={`#248094`} />
+         <span id="data">{data}</span>
+         <CopyIcon
+            color={`#248094`}
+            data={data}
+            width={'2.5rem'}
+            height={'2.5rem'}
+         />
       </div>
    );
 };
