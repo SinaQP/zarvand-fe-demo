e6d93e69f593c7a19b0acb29691ab36c821159d1
qasem <qasempoursina@gmail.com>
2025-04-15 08:36:22 +0330

    get the details from the url in payment status

--- src/pages/paymentStatus/components/paymentDetail/index.tsx
+++ src/pages/paymentStatus/components/paymentDetail/index.tsx
@@ -1,32 +1,40 @@
 import InfoRow from '../../../../components/infoRow';
 import styles from './index.module.scss';
-
+import { useSearchParams } from 'react-router-dom';
 const PaymentDetail = () => {
+   const [searchParams] = useSearchParams();
+
+   const billNo = searchParams.get('bill_no');
+   const payNo = searchParams.get('pay_no');
+   const amount = searchParams.get('amount');
+   const time = searchParams.get('time');
+   const traceNo = searchParams.get('trace_no');
+
    return (
       <>
          <InfoRow
             title="شناسه قبض:"
-            value="2620503195"
+            value={billNo || '-'}
             className={styles['payment-detail__row']}
          />
          <InfoRow
             title="شناسه پرداخت:"
-            value="2620503195"
+            value={payNo || '-'}
             className={styles['payment-detail__row']}
          />
          <InfoRow
             title="مبلغ پرداخت شده:"
-            value="2620503195"
+            value={amount || '-'}
             className={styles['payment-detail__row']}
          />
          <InfoRow
             title="زمان پرداخت:"
-            value="2620503195"
+            value={time || '-'}
             className={styles['payment-detail__row']}
          />
          <InfoRow
             title="کد پیگیری:"
-            value="2620503195"
+            value={traceNo || '-'}
             className={styles['payment-detail__row']}
          />
       </>
